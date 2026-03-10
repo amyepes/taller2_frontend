0d607bf19898d0f9092d5e3ef964314038211223
amyepes <amyepes@uninorte.edu.co>
2026-03-09 21:30:45 -0500

    Sticky navbar added

--- mi-app/src/App.jsx
+++ mi-app/src/App.jsx
@@ -7,17 +7,17 @@ function App() {
 
   return (
     <>
-      <header>
-        <nav className='navbar'>
-          <h5 className="logo">ReactAcademy</h5>
 
-          <ul className="nav-links">
-            <li><a href="#inicio" className="active">Inicio</a></li>
-            <li><a href="#cursos">Cursos</a></li>
-            <li><a href="#nosotros">Nosotros</a></li>
-          </ul>
-        </nav>
-      </header>
+      <nav className='navbar'>
+        <h5 className="logo">ReactAcademy</h5>
+
+        <ul className="nav-links">
+          <li><a href="#inicio" className="active">Inicio</a></li>
+          <li><a href="#cursos">Cursos</a></li>
+          <li><a href="#nosotros">Nosotros</a></li>
+        </ul>
+      </nav>
+
 
       <section id="inicio" className='hero'>
         <h1>Aprende <span>React</span> desde cero</h1>
@@ -29,10 +29,10 @@ function App() {
         <h2>Nuestros Cursos</h2>
         <p>Elige el camino que mejor se adapte a ti</p>
         <div className='tarjetas'>
-          <Card icon = '⚛️' titulo="React Básico" texto='Componentes, props, estado y eventos. Todo lo que necesitas para empezar.' boton='Principiante'/>
-          <Card icon = '🔄' titulo='React Hooks' texto='Profundiza en useState, useEffect y crea tus propios custom hooks.' />
-          <Card icon = '📁' titulo='Estado Global' texto='Gestiona el estado con Context API y aprende cuándo usarlo.' />
-          <Card icon = '🚀' titulo='React Avanzado' texto='Rendimiento, patrones avanzados y arquitectura para proyectos grandes.' boton='Avanzado'/>
+          <Card icon='⚛️' titulo="React Básico" texto='Componentes, props, estado y eventos. Todo lo que necesitas para empezar.' boton='Principiante' />
+          <Card icon='🔄' titulo='React Hooks' texto='Profundiza en useState, useEffect y crea tus propios custom hooks.' />
+          <Card icon='📁' titulo='Estado Global' texto='Gestiona el estado con Context API y aprende cuándo usarlo.' />
+          <Card icon='🚀' titulo='React Avanzado' texto='Rendimiento, patrones avanzados y arquitectura para proyectos grandes.' boton='Avanzado' />
         </div>
       </section>
 
